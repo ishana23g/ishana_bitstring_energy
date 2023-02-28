1cd38663d71fb80d07bc1905cf9f404fd0a2bbcb
ishana23g <ishana23g@vt.edu>
2023-02-28 11:13:38 -0500

    Initial commit after CMS Cookiecutter creation, version 1.1

---
+++ docs/api.rst
@@ -0,0 +1,7 @@
+API Documentation
+=================
+
+.. autosummary::
+   :toctree: autosummary
+
+   ishana_bitstring_energy.canvas
---
+++ ishana_bitstring_energy/tests/test_ishana_bitstring_energy.py
@@ -0,0 +1,15 @@
+"""
+Unit and regression test for the ishana_bitstring_energy package.
+"""
+
+# Import package, test suite, and other packages as needed
+import sys
+
+import pytest
+
+import ishana_bitstring_energy
+
+
+def test_ishana_bitstring_energy_imported():
+    """Sample test, will always pass so long as import statement worked."""
+    assert "ishana_bitstring_energy" in sys.modules
