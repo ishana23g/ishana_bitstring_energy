e6eb0b4c808bae50300650b38a89a23a1cef1d63
ishana23g <ishana23g@vt.edu>
2023-05-08 15:19:24 -0400

    wanted matplotlib for docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import matplotlib
+import numpy as np
 import bitstring_energy
 
 
@@ -88,7 +90,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
